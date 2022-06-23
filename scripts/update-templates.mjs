50d05b4182e24706134b65127dc455d7faedb750
Matt Kane <m@mk.gg>
2022-06-23 11:45:44 +0100

    Add skeleton

--- scripts/update-templates.mjs
+++ scripts/update-templates.mjs
@@ -1,8 +1,11 @@
+import { existsSync } from 'node:fs'
 import { cp, rm } from 'node:fs/promises'
 import { resolve } from 'node:path'
 const source = resolve('upstream', 'templates')
 const destination = resolve('templates')
-await rm(destination, { recursive: true })
+if (existsSync(destination)) {
+  await rm(destination, { recursive: true })
+}
 await cp(source, destination, {
   recursive: true,
 })
