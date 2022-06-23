8ee7bcb31da5c686239b12f0cc0b57d3a297e37f
Matt Kane <m@mk.gg>
2022-06-23 11:45:44 +0100

    Add skeleton

--- scripts/update-templates.mjs
+++ scripts/update-templates.mjs
@@ -1,8 +1,17 @@
+import { existsSync } from 'node:fs'
 import { cp, rm } from 'node:fs/promises'
 import { resolve } from 'node:path'
 const source = resolve('upstream', 'templates')
 const destination = resolve('templates')
-await rm(destination, { recursive: true })
+if (existsSync(destination)) {
+  await rm(destination, { recursive: true })
+}
+
+if (!existsSync(source)) {
+  console.error(`Could not find templates at ${source}`)
+  process.exit(1)
+}
+
 await cp(source, destination, {
   recursive: true,
 })
